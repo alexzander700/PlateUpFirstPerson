bec263de1db6a61b95158aff66af6ac3f3401b29
alexzander700 <ajhochreiter@gmail.com>
2022-12-12 15:56:04 -0500

    Is a note really needed?

--- Mod.cs
+++ Mod.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
 using System.Collections.Generic;
 using Kitchen;
 using KitchenMods;
@@ -6,8 +9,31 @@ using UnityEngine;
 using Unity.Collections;
 using Unity.Entities;
 using KitchenLib;
+using Cinemachine;
+using HarmonyLib;
 
 namespace FirstPersonMod
 {
+    public class firstPerson : GenericSystemBase, IModSystem
+    {
+        
 
+        protected override void Initialise()
+        {
+            base.Initialise();
+            CinemachineBrain[] brains = GameObject.FindObjectsOfType<CinemachineBrain>();
+            foreach(CinemachineBrain brain in brains)
+            {
+                brain.enabled = false;
+            }
+        }
+
+        
+
+        protected override void OnUpdate()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
 }
