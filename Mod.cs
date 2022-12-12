11d10eb1d3f84711d714923b8265d850164322c4
alexzander700 <ajhochreiter@gmail.com>
2022-12-12 09:37:07 -0500

    Redoing a lot of crap

--- Mod.cs
+++ Mod.cs
@@ -1,30 +1,13 @@
-﻿using BepInEx;
-using BepInEx.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using Kitchen;
+using KitchenMods;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
 using KitchenLib;
-using System.Reflection;
-using Cinemachine;
 
 namespace FirstPersonMod
 {
-    // guid must be unique and is recommended to be in reverse domain name notation
-    // mod name that is displayed to the player and listed in the mods menu
-    // mod version must follow semver e.g. "1.2.3"
-    [BepInPlugin("alexzander700.FirstPerson", "FirstPersonMod", "0.1.0")]
-    [BepInProcess("PlateUp.exe")]
-    [BepInDependency("kitchenmods.kitchenlib")]
-    public class DisableCinemachine : BaseUnityPlugin
-    {
-        private void Awake()
-        {
-            // Disable all Cinemachine components
-            var cinemachineComponents = FindObjectsOfType<CinemachineBrain>();
-            foreach (var component in cinemachineComponents)
-            {
-                component.enabled = false;
-            }
-        }
-    }
-
-
 
 }
